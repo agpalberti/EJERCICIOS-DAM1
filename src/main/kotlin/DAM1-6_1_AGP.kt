6362c0fb2d79d4af38bd1718167783bc32f5e10b
agpalberti <agonzalezp@iesrafaelalberti.es>
2022-01-18 03:29:24 +0100

    Falta especificacion

--- src/main/kotlin/DAM1-6_1_AGP.kt
+++ src/main/kotlin/DAM1-6_1_AGP.kt
@@ -1,11 +1,54 @@
+//Especialización
+
+//Superclase
+open class Trabajador(){
+    fun trabaja(){}
+}
+
+//Subclases
+class Guardia():Trabajador(){
+    fun vigila(){}
+}
+class Enfermero():Trabajador(){
+    fun opera(){}
+}
 
 
+//Extensión
+
+//Superclase
+open class Coche(){
+    fun acelera(){}
+}
+
+//Subclases
+class CocheVolador():Coche(){
+    fun vuela(){}
+}
+class CocheAcuatico():Coche(){
+    fun sumerje(){}
+}
+
+//Especificación
 
 
+//Construcción
 
+//Superclase
+open class Motor(){
+    fun aumentarRevoluciones(){}
+}
 
-
-
+//Subclases
+class Avion():Motor(){
+    fun vuela(){}
+}
+class Camion():Motor(){
+    fun acelerar(){}
+}
 
 fun main() {
+
+    val coche:Coche = CocheAcuatico()
+
 }
